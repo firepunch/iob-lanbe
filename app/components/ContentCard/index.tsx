aa163070db92e836c752200e4739bfc77f99d139
firepunch <firepunch119@gmail.com>
2023-07-14 21:52:55 +0900

    Add ContentCard component

---
+++ app/components/ContentCard/index.tsx
@@ -0,0 +1,40 @@
+import cls from 'classnames'
+import styles from './index.module.scss'
+
+interface ContentCardProps {
+  thumbnail_url?: string;
+  country?: string;
+  date?: string;
+  title?: string;
+  tags?: string[];
+  onClick?: () => void;
+  onSave?: () => void;
+}
+
+export const ContentCard = ({
+  thumbnail_url,
+  country = '',
+  date = '',
+  title = '',
+  tags = [],
+  onClick,
+  onSave,
+  ...props
+}: ContentCardProps) => {
+  return (
+    <div className={styles.card}>
+      {thumbnail_url ? 
+        <img src={thumbnail_url} alt="thumbnail" /> :
+        <img src="https://via.placeholder.com/200" alt="thumbnail" />
+      }
+      <div>
+        <span>{country}</span>
+        <span>{date}</span>
+      </div>
+      <p>{title}</p>
+      {tags.map(item=> (
+        <span key={item}>{item}</span>
+      ))}
+    </div>
+  )
+}
