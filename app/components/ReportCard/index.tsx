370fecdba38dc764eacba928b1f0e14f6e1e676a
firepunch <firepunch119@gmail.com>
2023-07-15 19:02:54 +0900

    Add ReportCard component

---
+++ app/components/ReportCard/index.tsx
@@ -0,0 +1,30 @@
+import cls from "classnames"
+import styles from "./index.module.scss"
+
+interface ReportCardProps {
+  thumbnail?: {sourceUrl:string; altText:string};
+  title?: string;
+  description?: string;
+  onClick?: () => void;
+  onSave?: () => void;
+}
+
+export const ReportCard = ({
+  thumbnail,
+  title = "",
+  description = "",
+  onClick,
+  onSave,
+  ...props
+}: ReportCardProps) => {
+  return (
+    <div className={styles.card}>
+      {thumbnail ? 
+        <img src={thumbnail.sourceUrl} alt={thumbnail.altText} /> :
+        <img src="https://via.placeholder.com/200" alt="thumbnail" />
+      }
+      <p>{title}</p>
+      <p>{description}</p>
+    </div>
+  )
+}
