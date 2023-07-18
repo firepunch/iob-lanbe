10d792332013e0065a711dd96471bddce7b9ced9
judyzero <qozmcll@kookmin.ac.kr>
2023-07-18 17:31:59 +0900

    feat(content option) scroll copy fontsize

--- app/[lang]/[content-name]/page.tsx
+++ app/[lang]/[content-name]/page.tsx
@@ -2,6 +2,8 @@
 
 import { getLocalePartsFrom, locales } from "i18n"
 import Link from "next/link"
+import React, { useState } from "react"
+import { LinkedinIcon, LinkedinShareButton, EmailShareButton, EmailIcon } from "react-share"
 
 export default function Category({
   params: { lang },
@@ -9,12 +11,19 @@ export default function Category({
   params: { lang: string; },
 }) {
 
-  const copyToClipboard = (e) => {
+  const copyURLButton = (e) => {
     navigator.clipboard.writeText(window.location.toString())
   }
 
+  const [prevZoomed, setZoomed] = useState(false)
+  const [fontSize, setFontSize] = useState(20)
+  const fontSizeButton = () => {
+    setZoomed(prevZoomed => !prevZoomed)
+    setFontSize(prevFontSize => (prevZoomed ? prevFontSize - 5 : prevFontSize + 5))
+  }
+
   return (
-    <main >
+    <main>
       <h2 id="content-header">Post Detail</h2>
 
       <div style={{ background: "black", height: "100vh" }}>
@@ -23,10 +32,23 @@ export default function Category({
       <Link href="#content-header">Scroll to top</Link>
 
       <div className="">
-        <button onClick={copyToClipboard}>
-          Copy
-        </button>
+        <button>share</button>
+        <div className="">
+          <button onClick={copyURLButton}>URL</button>
+          
+          <LinkedinIcon size={32} round />
+          <LinkedinShareButton url={window.location.toString()} title="title"> LinkedIn </LinkedinShareButton>
+          
+          <EmailIcon size={32} round />
+          <EmailShareButton url={window.location.toString()}> Mail </EmailShareButton>
+        </div>
       </div>
+
+      <div>
+        <p style={{ fontSize: `${fontSize}px` }}>This is some text for size test.</p>
+        <button onClick={fontSizeButton} title="handle Font Size"> Size </button>
+      </div>
+
     </main>
   )
 }
