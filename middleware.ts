eae7225cf891b1b54f27fec20525b51a80e4a931
firepunch <firepunch119@gmail.com>
2023-07-18 21:53:47 +0900

    Update middleware for redirect

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,39 @@
+import { match } from "@formatjs/intl-localematcher"
+import { defaultLocale, locales } from "i18n"
+import Negotiator from "negotiator"
+import { headers } from "next/headers"
 import { NextRequest, NextResponse } from "next/server"
+ 
+const getLocale = (req: NextRequest) => {
+  let headers = { "accept-language": "en-US,en;q=0.5" }
+  let languages = new Negotiator({ headers }).languages()
 
-const PUBLIC_FILE = /\.(.*)$/
+  return match(languages, locales as unknown as string [], defaultLocale) 
+}
 
-export async function middleware(req:NextRequest) {
-  if (
-    req.nextUrl.pathname.startsWith('/_next') ||
-    req.nextUrl.pathname.includes('/api/') ||
-    PUBLIC_FILE.test(req.nextUrl.pathname)
-  ) {
-    return
+export const middleware = (req: NextRequest) => {
+  const pathname = req.nextUrl.pathname
+  const pathnameIsMissingLocale = locales.every(
+    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
+  )
+
+  console.log(pathnameIsMissingLocale)
+
+  // Redirect if there is no locale
+  if (pathnameIsMissingLocale) {
+    const locale = "en" // getLocale(req)
+ 
+    return NextResponse.redirect(
+      new URL(`/${locale}/${pathname}`, req.url)
+    )
   }
+}
 
-  // if (req.nextUrl.locale === 'ko') {
-  //   return NextResponse.redirect(
-  //     new URL(`/ko${req.nextUrl.pathname}`, req.url)
-  //   )
-  // }
+export const config = {
+  matcher: [
+    // Skip all internal paths (_next)
+    "/((?!_next).*)",
+    // Optional: only run on root (/) URL
+    // '/'
+  ],
 }
