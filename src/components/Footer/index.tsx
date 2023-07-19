2c53412b4ffe2848bd9596b3b31e54d22e59710b
judyzero <qozmcll@kookmin.ac.kr>
2023-07-19 10:46:18 +0900

    Chore package update

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,42 @@
 import Link from "next/link"
+import LanguageSwitcher from "../LanguageSwitcher"
+import { ValidLocale, getTranslator } from "i18n"
 
-export default function Footer() {
+export default async function Footer( {
+  lang,
+}: {
+  lang: ValidLocale
+}){
+  const t = await getTranslator(lang as ValidLocale)
   return (
     <footer>
       Footer
       <Link href='/'>
         IOB
       </Link>
+      <nav>
+        <Link href={`/${lang}/content`}>
+          {t("menu.content")}
+        </Link>
+        <Link href={`/${lang}/report`}>
+          {t("menu.report")}
+        </Link>
+        {/* <Link href={`/${lang}/project`}>
+          {t("menu.project")}
+        </Link> */}
+        <Link href={`/${lang}/linkedin`}>
+          {t("menu.content")}
+        </Link>
+        <Link href={`/${lang}/search`}>
+          {t("menu.search")}
+        </Link>
+        <Link href={`/${lang}/login`}>
+          {t("menu.sign_in")}
+        </Link>
+        <Link href={`/${lang}/my-page`}>
+          {t("menu.my_page")}
+        </Link>
+      </nav>
     </footer>
   )
 }
