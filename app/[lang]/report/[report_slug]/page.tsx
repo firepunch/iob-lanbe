6ef85f62539bac5685fa9c50b9730c2385b6dc05
firepunch <firepunch119@gmail.com>
2023-07-19 15:16:06 +0900

    Update dynamic report path to slug using camel case

---
+++ app/[lang]/report/[report_slug]/page.tsx
@@ -0,0 +1,19 @@
+import { getProductBySlug } from '@/utils/api'
+import { ValidLocale, getTranslator } from 'i18n'
+
+export default async function Report({
+  params: { lang, report_slug },
+}: {
+  params: { lang: ValidLocale; report_slug: string; },
+}) {
+  const dict = await getTranslator(lang)
+  const reportData = getProductBySlug(report_slug)
+  const [report] = await Promise.all([reportData])
+
+  return (
+    <>
+      <h2>{dict.menu.about}</h2>
+      <p>{report.name}</p>
+    </>
+  )
+}
