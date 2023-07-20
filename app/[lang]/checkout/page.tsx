be763f92f19ac6e7e2980c4b42f11d90543067b6
firepunch <firepunch119@gmail.com>
2023-07-20 15:42:42 +0900

    Declare client component

--- app/[lang]/checkout/page.tsx
+++ app/[lang]/checkout/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { getTranslation } from '@/i18n/index'
 import { ValidLocale } from '@/i18n/settings'
 import { Elements } from '@stripe/react-stripe-js'
@@ -8,16 +10,15 @@ const stripePromise = loadStripe(
   process.env.STRIPE_SECRET_KEY as string
 )
 
-export default async function Checkout({
+export default function Checkout({
   params: { lang },
 }: {
   params: { lang: ValidLocale; },
 }) {
-  const { t } = await getTranslation(lang, 'second-page')
+  // const { t } = await getTranslation(lang, 'second-page')
 
   return (
     <>
-      <h2>{t('h1')}</h2>
       <p>Your order:</p>
       <Elements stripe={stripePromise}>
         <CheckoutForm />
