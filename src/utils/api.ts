9cc31f8c982fe19ec1ab2ba244e4ebc04645e5ee
firepunch <firepunch119@gmail.com>
2023-07-20 15:33:13 +0900

    Cast api url type

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -9,8 +9,8 @@ import {
 } from '@apollo/experimental-nextjs-app-support/ssr'
 import { registerApolloClient } from '@apollo/experimental-nextjs-app-support/rsc'
 
-const API_URL = process.env.WORDPRESS_API_URL
-
+const API_URL = process.env.WORDPRESS_API_URL as string
+ 
 export const { getClient } = registerApolloClient(() => {
   return new NextSSRApolloClient({
     cache: new NextSSRInMemoryCache(),
