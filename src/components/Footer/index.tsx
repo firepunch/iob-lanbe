6cf7f9a1eba7adca13068b3783383d6d45b380b7
firepunch <firepunch119@gmail.com>
2023-07-20 15:59:26 +0900

    Add footer locale

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,40 +1,24 @@
-import Link from "next/link"
-import LanguageSwitcher from "../LanguageSwitcher"
-import { ValidLocale, getTranslator } from "i18n"
+import Link from 'next/link'
+import { ValidLocale } from '@/i18n/settings'
+import { getTranslation } from '@/i18n/index'
 
 export default async function Footer( {
   lang,
 }: {
   lang: ValidLocale
 }){
-  const t = await getTranslator(lang as ValidLocale)
+  const { t } = await getTranslation(lang, 'footer')
+
   return (
     <footer>
       Footer
       <Link href="/">
         IOB
       </Link>
+
       <nav>
         <Link href={`/${lang}/content`}>
-          {t("menu.content")}
-        </Link>
-        <Link href={`/${lang}/report`}>
-          {t("menu.report")}
-        </Link>
-        {/* <Link href={`/${lang}/project`}>
-          {t("menu.project")}
-        </Link> */}
-        <Link href={`/${lang}/linkedin`}>
-          {t("menu.content")}
-        </Link>
-        <Link href={`/${lang}/search`}>
-          {t("menu.search")}
-        </Link>
-        <Link href={`/${lang}/login`}>
-          {t("menu.sign_in")}
-        </Link>
-        <Link href={`/${lang}/my-page`}>
-          {t("menu.my_page")}
+          {t('content')}
         </Link>
       </nav>
     </footer>
