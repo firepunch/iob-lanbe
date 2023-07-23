4ab5f1791501db342d42ca822ecb496e3ed834de
firepunch <firepunch119@gmail.com>
2023-07-23 16:19:10 +0900

    Delete next configure in api file

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,24 +1,9 @@
 import PRODUCTS_QUERY from '@/queries/products'
 import POSTS_QUERY from '@/queries/posts'
 import REPORT_BY_SLUG_QUERY from '@/queries/reportBySlug'
-import POST_BY_SLUG_QUERY from '@/queries/postBySlug'
-import { HttpLink } from '@apollo/client'
-import {
-  NextSSRInMemoryCache,
-  NextSSRApolloClient,
-} from '@apollo/experimental-nextjs-app-support/ssr'
-import { registerApolloClient } from '@apollo/experimental-nextjs-app-support/rsc'
+import { CHECKOUT_QUERY } from '@/queries/checkout'
 
 const API_URL = process.env.WORDPRESS_API_URL as string
- 
-export const { getClient } = registerApolloClient(() => {
-  return new NextSSRApolloClient({
-    cache: new NextSSRInMemoryCache(),
-    link: new HttpLink({
-      uri: process.env.WORDPRESS_API_URL,
-    }),
-  })
-})
 
 async function fetchAPI(query = '', { variables }: Record<string, any> = {}) {
   const headers = { 'Content-Type': 'application/json' }
