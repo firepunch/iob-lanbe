4d7a1dc8bf666c981e95a2a468999a6c41bdc4d1
firepunch <firepunch119@gmail.com>
2023-08-03 22:58:39 +0900

    Add refresh token query

--- src/queries/users.ts
+++ src/queries/users.ts
@@ -1,20 +1,31 @@
 export const LOGIN_QUERY = `
 mutation LoginUser($input: LoginInput!) {
   login(input: $input) {
-    authToken
     clientMutationId
+    authToken
     refreshToken
     sessionToken
   }
-}`
+}
+`
 
 export const REGISTER_QUERY = `
 mutation RegisterUser($input: RegisterUserInput!) {
   registerUser(input: $input) {
+    clientMutationId
     user {
       jwtAuthToken
       jwtRefreshToken
     }
+  }
+}
+`
+
+export const REFRESH_TOKEN_QUERY = `
+mutation RefreshAuthToken($refreshToken: String!) {
+  refreshJwtAuthToken(input: { jwtRefreshToken: $refreshToken }) {
+    authToken
+    sessionToken
   }
 }
 `
