4dcadaba142fe320c59c2fc9152aaa96a62ed725
firepunch <firepunch119@gmail.com>
2023-08-15 01:52:37 -0700

    Render additional data on category

--- app/[lang]/category/page.tsx
+++ app/[lang]/category/page.tsx
@@ -1,56 +1,24 @@
-import { getContentsByCategory } from '@/api_gql'
-import { Icons, Select } from '@/components'
-import { getTranslation } from '@/i18n/index'
-import { ValidLocale } from '@/i18n/settings'
 import Image from 'next/image'
 import Link from 'next/link'
-
+import { getContentsByCategory } from '@/api_gql'
+import { Icons, PostCard, Select, Pagination } from '@/components'
+import { getTranslation } from '@/i18n/index'
+import { ValidLocale } from '@/types'
 import ArrowBlackDown from '@/imgs/arrow_black_down.png'
 
 export default async function Category({
   params: { lang },
-  searchParams: { name },
+  searchParams,
 }: {
-  params: { lang: ValidLocale; },
-  searchParams: { name?: string }
+  params: { lang: ValidLocale }
+  searchParams
 }) {
-  /**
-  const categoryData = getContentsByCategory(nameQueryParam)
-  const [category] = await Promise.all([categoryData])
-  {category?.posts?.edges?.map(async ({ node }) => {
-        console.log(node.title)
-        // <p key={node.id}> {node.title}</p>
-      })}
-  */
-  
   const { t: ct } = await getTranslation(lang, 'common')
   const { t } = await getTranslation(lang, 'category-page')
-  const categoryName = name || 'all'
+  const categoryName = searchParams?.name || 'all'
 
   const postData = getContentsByCategory(categoryName)
   const [posts] = await Promise.all([postData])
-  console.log(posts)
-
-  // category?.map(async ({ node }) => {
-  //   if (node.name == nameQueryParam){
-  //     const categoryId = node.id
-  //     const ContentsByCategoryData = getContentsByCategory(categoryId)
-  //     const [contents] = await Promise.all([ContentsByCategoryData])
-  //     console.log(contents)
-  //     {contents.posts?.edges?.map(({ node }) => (
-  //       console.log(node.title)
-  //       // <Link 
-          
-  //       //   key={node.id} 
-  //       //   href={`/${encodeURIComponent(node.slug)}`}>
-  //       //   <ContentCard
-  //       //     thumbnail_url={node.featuredImage?.node.sourceUrl}
-  //       //     {...node}
-  //       //   />
-  //       // </Link>
-  //     ))}
-  //   }
-  // })
 
   return (
     <>
@@ -160,6 +128,13 @@ export default async function Category({
       {/* grid */}
       <section id="contents-grid">
         <div id="all-contents-wrap">
+          {posts?.length && posts.map(({ node }) => (
+            <PostCard
+              key={node.id}
+              thumbnail_url={node.featuredImage?.node}
+              {...node}
+            />
+          ))}
         </div>
 
         {/* pagination */}
--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -1,28 +1,38 @@
 import Image from 'next/image'
+import Link from 'next/link'
+import { dateFormat } from '@/utils/lib'
 
 import SaveImg from '@/imgs/save.png'
 import LocationBlackImg from '@/imgs/locationicon_black.png'
 
 interface PostCardProps {
-  thumbnail?: {sourceUrl:string; altText:string};
-  title?: string;
-  description?: string;
+  thumbnail?: {sourceUrl: string; altText: string};
+  tags?: {nodes: {id: string; name: string}[]};
+  lanbePost: {is_save: boolean; country: string};
+  slug: string;
+  title: string;
+  date: string;
+  country: string;
   onClick?: () => void;
   onSave?: () => void;
 }
 
 export const PostCard = ({
   thumbnail,
+  tags,
+  slug,
   title = '',
-  description = '',
+  date = '',
+  lanbePost: { is_save, country },
   onClick,
   onSave,
   ...props
 }: PostCardProps) => {
   return (
-    <>
-      <div className="indiv-content i-c-1">
+    <Link href={`/${encodeURIComponent(slug)}`}>
+      <div className="indiv-content">
         <div className="thumbnail">
+          {thumbnail && <Image src={thumbnail.sourceUrl} alt={thumbnail.altText} />}
           <div className="save">
             <Image src={SaveImg} alt="Save" />
           </div>
@@ -31,31 +41,28 @@ export const PostCard = ({
         <div className="location-date">
           <div className="country">
             <Image src={LocationBlackImg} alt="Location icon" />
-            <p>COUNTRY</p>
+            <p>{country.toUpperCase()}</p>
           </div>
 
-          <p className="date">23.07.25</p>
+          <p className="date">
+            {dateFormat(date)}
+          </p>
         </div>
 
-        <a href="#" className="indiv-content-title">
-          Title sample: Product Placement Strategy Revived a 35 year-old Shoe Brand
-        </a>
+        <p className="indiv-content-title">
+          {title}
+        </p>
 
-        <div className="tags">
-          <div className="indiv-tag">Tag</div>
-          <div className="indiv-tag">Long Tag</div>
-        </div>
+        {tags?.nodes?.length && (
+          <div className="tags">
+            {tags.nodes.map(item => (
+              <div key={item.id} className="indiv-tag">
+                {item.name}
+              </div>
+            ))}
+          </div>
+        )}
       </div>
-
-      <div>
-        {thumbnail ? 
-          <img src={thumbnail.sourceUrl} alt={thumbnail.altText} /> :
-          <img src="https://via.placeholder.com/200" alt="thumbnail" />
-        }
-        <p>{title}</p>
-        <p>{description}</p>
-      </div>
-
-    </>
+    </Link>
   )
 }
