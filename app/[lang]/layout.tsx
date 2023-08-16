b0fa87d3ac0c5a6efefb31ef37febcde37c0ec46
firepunch <firepunch119@gmail.com>
2023-08-16 07:15:21 -0700

    Convert Layout to client component

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,5 +1,8 @@
+'use client'
+
 import { Footer, Header } from '@/components'
 import { getTranslation } from '@/i18n/index'
+import { useTranslation } from '@/i18n/client'
 import { ValidLocale, languages } from '@/i18n/settings'
 import { sendEmailForm } from '@/api_wp'
 import { useState } from 'react'
@@ -9,15 +12,19 @@ export function generateStaticParams() {
   return languages.map((lang) => ({ lang }))
 }
 
-export default async function LocaleLayout({
+const NO_HEADER_MAP = [
+  'sign-in',
+]
+
+export default function LocaleLayout({
   children,
   params: { lang },
 }: {
   children: React.ReactNode,
   params: { lang: ValidLocale }
 }) {
-  const { t: ct } = await getTranslation(lang, 'category-page')
-  const { t } = await getTranslation(lang, 'layout')
+  const { t: ct } = useTranslation(lang, 'category-page')
+  const { t } = useTranslation(lang, 'layout')
   // const [errorCode, setErrorCode] = useState()
 
   const handleSubmit = async (email: string) => {
