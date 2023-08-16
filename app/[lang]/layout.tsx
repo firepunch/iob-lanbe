f8d8f07b804301cdeef48c532516f1f03fcd2b80
firepunch <firepunch119@gmail.com>
2023-08-16 08:47:28 -0700

    Render SimpleHeader on login screen

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,30 +1,29 @@
-'use client'
-
-import { Footer, Header } from '@/components'
-import { getTranslation } from '@/i18n/index'
+import { sendEmailForm } from '@/api_wp'
+import { Footer, Header, SimpleHeader } from '@/components'
+import { getTranslation } from '@/i18n'
 import { useTranslation } from '@/i18n/client'
 import { ValidLocale, languages } from '@/i18n/settings'
-import { sendEmailForm } from '@/api_wp'
-import { useState } from 'react'
-import { EmailForm } from '@/components'
+import { headers } from 'next/headers'
 
 export function generateStaticParams() {
   return languages.map((lang) => ({ lang }))
 }
 
-const NO_HEADER_MAP = [
+const SIMPLE_HEADER_MAP = [
   'sign-in',
 ]
 
-export default function LocaleLayout({
+export default async function LocaleLayout({
   children,
   params: { lang },
 }: {
   children: React.ReactNode,
   params: { lang: ValidLocale }
 }) {
-  const { t: ct } = useTranslation(lang, 'category-page')
-  const { t } = useTranslation(lang, 'layout')
+  const headersList = headers()
+  const fullUrl = headersList.get('referer') || ''
+  const { t: ct } = await getTranslation(lang, 'category-page')
+  const { t } = await getTranslation(lang, 'layout')
   // const [errorCode, setErrorCode] = useState()
 
   const handleSubmit = async (email: string) => {
@@ -43,7 +42,10 @@ export default function LocaleLayout({
     <html lang={lang}>
       <head />
       <body>
-        <Header lang={lang} ct={ct} t={t} />
+        {SIMPLE_HEADER_MAP.find(item=> fullUrl.includes(item)) ?
+          <SimpleHeader lang={lang} /> :
+          <Header lang={lang} ct={ct} t={t} /> 
+        }
         <main>
           {children}
         </main>
@@ -57,3 +59,5 @@ export default function LocaleLayout({
     </html>
   )
 }
+
+export const dynamic = 'force-dynamic'
