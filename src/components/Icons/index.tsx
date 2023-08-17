23618f4658342214186426a9abc9c3e43dbf63d1
judyzero <qozmcll@kookmin.ac.kr>
2023-08-17 16:24:45 +0900

    merge html(inquiry form 빼고)

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 
 import ArrowBlack from '@/imgs/arrow_black.png'
+import ArrowBlackDown from '@/imgs/arrow_black_down.png'
 import ArrowWhite from '@/imgs/arrow_white.png'
 import SaveIcon from '@/imgs/save.png'
 import LocationIcon from '@/imgs/locationicon_black.png'
@@ -9,10 +10,11 @@ import LocationIcon from '@/imgs/locationicon_black.png'
 export default function Icons({
   type,
 }: {
-  type: 'arrowBlack' | 'arrowWhite' | 'save' | 'location'
+  type: 'arrowBlack' | 'arrowBlackDown' |'arrowWhite' | 'save' | 'location'
 }) {
   const IMAGE_MAP = {
     arrowBlack: ArrowBlack,
+    arrowBlackDown: ArrowBlackDown,
     arrowWhite: ArrowWhite,
     save: SaveIcon,
     location: LocationIcon,
