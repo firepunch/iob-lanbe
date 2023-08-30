2bb12230208b8bec656cab4284b592d3fe9158fb
firepunch <firepunch119@gmail.com>
2023-08-29 17:58:27 -0700

    Prevent bookmark without login

--- src/components/Bookmark/index.tsx
+++ src/components/Bookmark/index.tsx
@@ -4,6 +4,7 @@ import SaveImg from '@/imgs/save.png'
 import SavedImg from '@/imgs/saved.png'
 import SaveBlackImg from '@/imgs/save_black.png'
 import SavedBlackImg from '@/imgs/saved_black.png'
+import { isValidToken } from '@/utils/lib'
 
 export default function Bookmark({
   isBlack = false,
@@ -20,13 +21,20 @@ export default function Bookmark({
     isSaved ? SavedImg : SaveImg
   )
 
+  const handleClick = e => {
+    e.preventDefault() 
+    
+    if (isValidToken()) {
+      onToggle()
+    } else {
+      alert('로그인이 필요한 기능입니다.')
+    }
+  }
+
   return (
     <div 
       className="save"
-      onClick={e=> {
-        e.preventDefault() 
-        onToggle()
-      }}
+      onClick={handleClick}
     >
       <Image src={imgSrc} alt="Save" />
     </div>
