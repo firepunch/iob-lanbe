38ac3de46de02088ba55e88993a077b630288ab5
firepunch <firepunch119@gmail.com>
2023-08-29 21:16:24 -0700

    Update linkedin link

--- app/[lang]/about/page.tsx
+++ app/[lang]/about/page.tsx
@@ -102,7 +102,7 @@ export default async function About({
                     </div>
                   </div>
           
-                  <Link href="https://kr.linkedin.com/in/junha-son-399550115?trk=org-employees">
+                  <Link href="https://www.linkedin.com/in/junha-son-399550115">
                     <Image src={linkedInIcon} alt="LinkedIn" />
                   </Link>
                 </div>
