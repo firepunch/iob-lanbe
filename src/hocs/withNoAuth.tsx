6f0ed4c9ee3f574718dbf7a965965f6228647ae0
firepunch <firepunch119@gmail.com>
2023-09-05 16:38:58 -0700

    Refactoring NoAuth HOC

--- src/hocs/withNoAuth.tsx
+++ src/hocs/withNoAuth.tsx
@@ -1,15 +1,27 @@
+import useUserState from '@/stores/userStore'
+import { IResponseUser } from '@/types/store'
 import { NextComponentType } from 'next'
 import { useRouter } from 'next/navigation'
-import { isValidToken } from '../utils/lib'
+import { useEffect } from 'react'
+import { AUTH_TOKEN, getStorageData } from '../utils/lib'
 
 const withNoAuth = (Component: NextComponentType) => {
   const Auth = props => {
-    const isValid = isValidToken()
+    const { user, updateUser } = useUserState(state => state)
+    const [storageUser] = getStorageData(AUTH_TOKEN)
     const { back } = useRouter()
 
-    return isValid ? 
-      back() :
-      <Component {...props} />
+    useEffect(() => {
+      if (storageUser && !user) {
+        updateUser(storageUser as IResponseUser)
+      }
+      
+      if (storageUser || user) {
+        back()
+      }
+    }, [user, storageUser, updateUser, back])
+
+    return <Component {...props} />
   }
 
   return Auth
