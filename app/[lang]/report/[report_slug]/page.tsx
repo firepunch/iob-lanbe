b61ce2c5d2c8b6c3fc283e6b417aab1bcabc0855
firepunch <firepunch119@gmail.com>
2023-09-04 17:11:26 -0700

    Update PostOptions

--- app/[lang]/report/[report_slug]/page.tsx
+++ app/[lang]/report/[report_slug]/page.tsx
@@ -77,135 +77,136 @@ export default function Report({
   }
 
   return (
-    <div className="iob-single-report">
-      <section id="report-firstpage">
-        <div id="report-firstpage-left">
-          <div className="save-share">
-            <Bookmark 
-              isSaved={report.lanbeContent.is_save}
-              onToggle={() => (
-                handleToggleBookmark({ 
-                  isSaved: report.lanbeContent.is_save,
-                  databaseId: report.databaseId,
-                })
-              )}
-            />
-            <Image src={ShareImg} alt="Share" />
+    <>
+      <PostOptions
+        isSaved={report.lanbeContent.is_save}
+        onFontSize={() => {}} 
+        onToggleBookmark={() => (
+          handleToggleBookmark({
+            isSaved: report?.lanbeContent.is_save,
+            databaseId: report.databaseId,
+          })
+        )}
+      />
+      <div className="iob-single-report">
+        <section id="report-firstpage">
+          <div id="report-firstpage-left">
+            <div className="save-share">
+              <Bookmark 
+                isSaved={report.lanbeContent.is_save}
+                onToggle={() => (
+                  handleToggleBookmark({ 
+                    isSaved: report.lanbeContent.is_save,
+                    databaseId: report.databaseId,
+                  })
+                )}
+              />
+              <Image src={ShareImg} alt="Share" />
+            </div>
+
+            <h2>{report.title}</h2>
+
+            {report.reportTags?.edges && (
+              <Tags lang={lang} tags={report.reportTags} />
+            )}
+
+            <div className="report-details">
+              <ul>
+                <li>{t('writer')}</li>
+                <li>{t('date')}</li>
+                <li>{t('category')}</li>
+                <li>{t('pages')}</li>
+              </ul>
+
+              <ul>
+                <li>{getAuthorInfo(report.author)}</li>
+                <li>{dateFormat(report.date, true)}</li>
+                <li>
+                  {report.reportCategories.edges?.map(({ node }) => node.name).join(', ')}
+                </li>
+                <li>
+                  {report.lanbeContent.pages}
+                </li>
+              </ul>
+            </div>
           </div>
+          <div id="report-firstpage-right">
+            <h4>Why it matters?</h4>
+            <p>{report.lanbeContent?.whyItMatters}</p>
+          </div>
+        </section>
 
-          <h2>{report.title}</h2>
-
-          {report.reportTags?.edges && (
-            <Tags lang={lang} tags={report.reportTags} />
+        <section id="report-snippets">
+          {report.lanbeContent?.firstImage && (
+            <Image 
+              src={report.lanbeContent.firstImage} 
+              alt="Report Image"
+              fill
+              sizes="100vw"
+            />
           )}
 
-          <PostOptions
-            isSaved={report.lanbeContent.is_save}
-            onFontSize={() => {}} 
-            onToggleBookmark={() => (
-              handleToggleBookmark({
-                isSaved: report?.lanbeContent.is_save,
-                databaseId: report.databaseId,
-              })
-            )}
-          />
-
-          <div className="report-details">
-            <ul>
-              <li>{t('writer')}</li>
-              <li>{t('date')}</li>
-              <li>{t('category')}</li>
-              <li>{t('pages')}</li>
-            </ul>
-
-            <ul>
-              <li>{getAuthorInfo(report.author)}</li>
-              <li>{dateFormat(report.date, true)}</li>
-              <li>
-                {report.reportCategories.edges?.map(({ node }) => node.name).join(', ')}
-              </li>
-              <li>
-                {report.lanbeContent.pages}
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div id="report-firstpage-right">
-          <h4>Why it matters?</h4>
-          <p>{report.lanbeContent?.whyItMatters}</p>
-        </div>
-      </section>
-
-      <section id="report-snippets">
-        {report.lanbeContent?.firstImage && (
-          <Image 
-            src={report.lanbeContent.firstImage} 
-            alt="Report Image"
-            fill
-            sizes="100vw"
-          />
-        )}
-
-        <div id="report-image-text1">
-          {report.lanbeContent?.secondImage && (
-            <Image 
-              src={report.lanbeContent.secondImage} 
-              alt="Report Image"
-              fill
-              sizes="100vw"
-            />
-          )}   
-          <div className="rit-text">
-            <p>{report.lanbeContent?.secondText}</p>
-          </div>
-        </div>
-
-        <div id="report-image-text2">
-          <div className="rit-text">
-            <p>{report.lanbeContent?.thirdText}</p>
-          </div>
-          {report.lanbeContent?.thirdImage && (
-            <Image 
-              src={report.lanbeContent.thirdImage} 
-              alt="Report Image"
-              fill
-              sizes="100vw"
-            />
-          )}   
-        </div>
-      </section>
-
-      {/* section 3: report price and cta */}
-      <section id="report-price-cta">
-        <div id="report-price-cta-wrap">
-          <div className="report-title">
-            <h4>{report.title}</h4>
+          <div id="report-image-text1">
+            {report.lanbeContent?.secondImage && (
+              <Image 
+                src={report.lanbeContent.secondImage} 
+                alt="Report Image"
+                fill
+                sizes="100vw"
+              />
+            )}   
+            <div className="rit-text">
+              <p>{report.lanbeContent?.secondText}</p>
+            </div>
           </div>
 
-          <div className="report-cta">
-            {isValidToken() ? (
-              <>
-                <p>{t('download_cta')}</p>
-                <Link 
-                  href={report.lanbeContent?.downloadFile || ''} 
-                  className="cta-link" 
-                  target="_blank"
-                  onClick={handleUpdateCount}
-                >
-                  {t('download')}
-                </Link>
-              </>
-            ) : (
-              <>
-                <p>{t('sign_in_cta')}</p>
-                <Link href={`/${lang}/sign-in`}>
-                  {t('sign_in')}
-                </Link>
-              </>
-            )}
+          <div id="report-image-text2">
+            <div className="rit-text">
+              <p>{report.lanbeContent?.thirdText}</p>
+            </div>
+            {report.lanbeContent?.thirdImage && (
+              <Image 
+                src={report.lanbeContent.thirdImage} 
+                alt="Report Image"
+                fill
+                sizes="100vw"
+              />
+            )}   
           </div>
-        </div>
-      </section>
-    </div>
+        </section>
+
+        {/* section 3: report price and cta */}
+        <section id="report-price-cta">
+          <div id="report-price-cta-wrap">
+            <div className="report-title">
+              <h4>{report.title}</h4>
+            </div>
+
+            <div className="report-cta">
+              {isValidToken() ? (
+                <>
+                  <p>{t('download_cta')}</p>
+                  <Link 
+                    href={report.lanbeContent?.downloadFile || ''} 
+                    className="cta-link" 
+                    target="_blank"
+                    onClick={handleUpdateCount}
+                  >
+                    {t('download')}
+                  </Link>
+                </>
+              ) : (
+                <>
+                  <p>{t('sign_in_cta')}</p>
+                  <Link href={`/${lang}/sign-in`}>
+                    {t('sign_in')}
+                  </Link>
+                </>
+              )}
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
   )
 }
