a149ffe3c2835853618bed51a003f868f7ff968c
firepunch <firepunch119@gmail.com>
2023-09-06 19:00:27 -0700

    call watchlist api in Bookmark

--- src/components/Bookmark/index.tsx
+++ src/components/Bookmark/index.tsx
@@ -6,17 +6,24 @@ import SaveImg from '@/imgs/save.png'
 import SaveBlackImg from '@/imgs/save_black.png'
 import SavedImg from '@/imgs/saved.png'
 import SavedBlackImg from '@/imgs/saved_black.png'
+import { createWatchList, removeWatchList } from '@/api_wp'
+import { useState } from 'react'
 
 export default function Bookmark({
   isBlack = false,
   isSaved = false,
-  onToggle,
+  metaKey = '',
+  contentId = 0,
+  onFetchData,
 }: {
   isBlack?: boolean
-  isSaved?: boolean
-  onToggle: () => void
+  isSaved: boolean
+  metaKey: string
+  contentId: number
+  onFetchData: () => void
 }) {
-  const { user } = useUserState(state=>state)
+  const [isProcess, setIsProcess] = useState(false)
+  const { user } = useUserState(state => state)
   const router = useRouter()
   const params = useParams()
   const imgSrc = isBlack ? (
@@ -25,21 +32,40 @@ export default function Bookmark({
     isSaved ? SavedImg : SaveImg
   )
 
-  const handleClick = e => {
-    e.preventDefault() 
+  const handleClick = async (e: React.FormEvent<HTMLDivElement>) => {
+    e.preventDefault()
+    if (isProcess) return
+    setIsProcess(true)
     
-    if (user.databaseId) {
-      onToggle()
+    if (user.databaseId && contentId) {
+      try {
+        if (isSaved) {
+          await removeWatchList({
+            type: metaKey,
+            content_id: contentId,
+            user_id: user.databaseId,
+          })
+        } else {
+          await createWatchList({
+            type: metaKey,
+            content_id: contentId,
+            user_id: user.databaseId,
+          })
+        }
+
+        if (onFetchData) onFetchData()
+      } catch (err) {
+        console.log(err)
+      }
     } else {
       router.replace(`/${params?.lang || 'en'}/sign-in`)
     }
+
+    setIsProcess(false)
   }
 
   return (
-    <div 
-      className="save"
-      onClick={handleClick}
-    >
+    <div className="save" onClick={handleClick}>
       <Image src={imgSrc} alt="Save" />
     </div>
   )
