fefad75603ce16266fde912386bdc0dcaf298cea
firepunch <firepunch119@gmail.com>
2023-09-10 16:53:29 -0700

    Update head tag

--- app/[lang]/head.tsx
+++ app/[lang]/head.tsx
@@ -1,9 +1,11 @@
+import Head from 'next/head'
+
 export default function HtmlHead() {
   return (
-    <head>
+    <Head>
       <title>Ideas on board</title>
       <meta name="description" content="Find your business opportunity" />
       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
-    </head>
+    </Head>
   )
 }
