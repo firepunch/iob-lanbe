cbfde7cb42b2dbe51272a295bc87cfb27c0dd304
firepunch <firepunch119@gmail.com>
2023-09-14 18:15:57 -0700

    Multiple favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,12 @@ export const metadata: Metadata = {
     description: 'I.O.B offers curated business content and professional services that help you enter the Southeast Asian market.',
     images: 'https://i0.wp.com/api.iob.team/wp-content/uploads/2023/09/link_thumbnail.jpg',
   },
+  icons: [
+    { rel: 'icon', sizes: '32x32', url: '/favicon/favicon-32x32.png' },
+    { rel: 'icon', sizes: '16x16', url: '/favicon/favicon-16x16.png' },
+    { rel: 'icon', sizes: '180x180', url: '/favicon/apple-touch-icon.png' },
+    { rel: 'icon', sizes: '96x96', url: '/favicon/android-chrome-96x96.png' },
+  ],
 }
 
 export default function RootLayout({
