0820aeadea3aeea35d756a508b57afb1f77dcd37
firepunch <firepunch119@gmail.com>
2023-10-26 18:48:31 -0700

    Update sitemap

--- app/[lang]/sitemap.ts
+++ app/[lang]/sitemap.ts
@@ -1,24 +1,24 @@
-import { getSitemapUrls } from '@/api_gql'
-import { MetadataRoute } from 'next'
+// import { getSitemapUrls } from '@/api_gql'
+// import { MetadataRoute } from 'next'
  
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const WEB_URL = process.env.NEXT_PUBLIC_WORDPRESS_URL || 'https://iob.team'
-  const enPosts = await getSitemapUrls({
-    language: 'EN', 
-  })
-  const koPosts = await getSitemapUrls({
-    language: 'KO', 
-  })
+// export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+//   const WEB_URL = process.env.NEXT_PUBLIC_WORDPRESS_URL || 'https://iob.team'
+//   const enPosts = await getSitemapUrls({
+//     language: 'EN', 
+//   })
+//   const koPosts = await getSitemapUrls({
+//     language: 'KO', 
+//   })
 
-  const enPostUrls = enPosts?.map(post => ({
-    url: `${WEB_URL}/en/content/${post?.slug}`,
-    lastModified: new Date(post?.dateGmt),
-  }))  
+//   const enPostUrls = enPosts?.map(post => ({
+//     url: `${WEB_URL}/en/content/${post?.slug}`,
+//     lastModified: new Date(post?.dateGmt),
+//   }))  
 
-  const koPostUrls = koPosts?.map(post => ({
-    url: `${WEB_URL}/ko/content/${post?.slug}`,
-    lastModified: new Date(post?.dateGmt),
-  }))  
+//   const koPostUrls = koPosts?.map(post => ({
+//     url: `${WEB_URL}/ko/content/${post?.slug}`,
+//     lastModified: new Date(post?.dateGmt),
+//   }))  
 
-  return [...enPostUrls, ...koPostUrls]
-}
+//   return [...enPostUrls, ...koPostUrls]
+// }
